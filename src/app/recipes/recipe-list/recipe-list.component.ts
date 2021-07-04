65f89c99668bd27ade497d0e1889295381c8ce1a
Benjlr <roberts.benjamin.luke@gmail.com>
2021-07-04 20:28:36 +1000

    fg

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
   recipes : Recipe[]=[
-    new Recipe("donuts", "How to make Donuts!", "https://www.littlenation.com.au/wp-content/uploads/2020/07/FVW2awFQ.jpeg"),
+    new Recipe("donuts", "How to make Donuts!", "https://static01.nyt.com/images/2014/12/14/magazine/14eat1/14mag-14eat.t_CA0-articleLarge.jpg"),
     new Recipe("Roast Beef", "How to make Roast Beef!", "https://grumpyshoneybunch.com/wp-content/uploads/2020/01/Air-Fryer-Roast-Beef3.jpg.webp")
   ];
 
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+this.recipeWasSelected.emit(recipe);
+  }
 }
